f05376f66658f670dff7fbf3d3bd3890b43282f5
alexanderzou <azou00@stuy.edu>
2020-05-01 21:47:21 -0400

    progress

--- script.py
+++ script.py
@@ -47,5 +47,78 @@ def run(filename):
     reflect = '.white'
 
     print(symbols)
+    print(stack)
     for command in commands:
         print(command)
+        
+    points = []
+    clear_screen(screen)
+    clear_zbuffer(zbuffer)
+    
+    for command in commands:
+        op = command['op']
+        if op == 'push':
+            stack.append( [x[:] for x in stack[-1]] )
+        elif op == 'pop':
+            stack.pop()
+        elif op == 'move':
+            tmp = make_translate(float(command['args'][0]), float(command['args'][1]), float(command['args'][2]))
+            matrix_mult( stack[-1], tmp )
+            stack[-1] = [ x[:] for x in tmp]
+            tmp = []
+        elif op == 'scale':
+            tmp = make_scale(float(command['args'][0]), float(command['args'][1]), float(command['args'][2]))
+            matrix_mult( stack[-1], tmp )
+            stack[-1] = [ x[:] for x in tmp]
+            tmp = []
+        elif op == 'rotate':
+            if command['args'][0] == 'x':
+                tmp = make_rotX(command['args'][1])
+            elif command['args'][0] == 'y':
+                tmp = make_rotY(command['args'][1])
+            else:
+                tmp = make_rotZ(command['args'][1])
+            matrix_mult( stack[-1], tmp )
+            stack[-1] = [ x[:] for x in tmp]
+            tmp = []
+        elif op == 'sphere':
+            if command['constants'] == None:
+                ref = '.white'
+            else:
+                ref = command['constants']
+            val = command['args']
+            add_sphere(points, val[0], val[1], val[2], val[3], step_3d)
+            matrix_mult( stack[-1], points )
+            draw_polygons(points, screen, zbuffer, view, ambient, light, symbols, ref)
+            points = []
+        elif op == 'torus':
+            if command['constants'] == None:
+                ref = '.white'
+            else:
+                ref = command['constants']
+            val = command['args']
+            add_torus(points, val[0], val[1], val[2], val[3], val[4], step_3d)
+            matrix_mult( stack[-1], points )
+            draw_polygons(points, screen, zbuffer, view, ambient, light, symbols, ref)
+            points = []
+        elif op == 'box':
+            if command['constants'] == None:
+                ref = '.white'
+            else:
+                ref = command['constants']
+            val = command['args']
+            add_box(points, val[0], val[1], val[2], val[3], val[4], val[5])
+            matrix_mult( stack[-1], points )
+            draw_polygons(points, screen, zbuffer, view, ambient, light, symbols, ref)
+            points = []
+        elif op == 'line':
+            val = command['args']
+            add_edge(points, val[0], val[1], val[2], val[3], val[4], val[5])
+            matrix_mult( stack[-1], points )
+            draw_lines(points, screen, zbuffer, color)
+            points = []
+        elif op == 'save':
+            print(command['args'][0]+'.png')
+            save_extension(screen, command['args'][0]+'.png')
+        elif op == 'display':
+            display(screen)
